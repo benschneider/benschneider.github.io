8dc86da76836b42d12c1f14b5f7e07b646a485ae
Dr. Ben Schneider <benschneider@users.noreply.github.com>
2023-10-21 22:24:24 -0700

    testing an egui template.

---
+++ dist/sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'secplorer1-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './secplorer1.js',
+  './secplorer1_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
